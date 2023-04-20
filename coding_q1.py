00afbaa1eeb36f9287360a1a56b07599321ddb28
Erin Bennett <ebennett@kehillah.org>
2023-04-20 05:33:27 -0700

    coding questions

---
+++ coding_q1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Cat():
+    """a cat"""
+    def speak(self):
+        # I'm being cute with the emoji here. Delete it if you get errors.
+        print("Meow!🐱")
+
+ella = Cat()
+ella.speak()
+
+# Write a new class `Dog` with its own `speak` method
+#### YOUR CODE HERE ####
+
+# Make a new object of class `Dog` and call its `speak` method
+#### YOUR CODE HERE ####
+
---
+++ coding_q2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Cat():
+    """a cat"""
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        # I'm being cute with the emoji here. Delete it if you get errors.
+        print(f"{self.name} says meow!🐱")
+
+ella = Cat("Ella")
+ella.speak()
+
+# Write a new class `Dog`.
+# Your class should have its own `__init__` method that sets a property `name`.
+# Your class should have its own `speak` method that uses its name.
+#### YOUR CODE HERE ####
+
+# Make a new object of class `Dog` and call its `speak` method
+#### YOUR CODE HERE ####
+
